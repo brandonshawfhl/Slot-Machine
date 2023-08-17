799a8c4e3df1e374d26d76c36bd8c6133a479e38
brandonshawfhl <142125786+brandonshawfhl@users.noreply.github.com>
2023-08-16 18:07:18 -0700

    populated lists topLine, middleLine & bottomLine
    changes the print of the slotMachine to print from the lists topLine, middleLine & bottomLine

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -18,23 +18,34 @@
                 slotMachine[verticalNumber, 2] = rng.Next(0, RANDOM_MAX);
             }
 
-            List<int> topLine = new List<int>();  
+            List<int> topLine = new List<int>();
+            // I couldn't figure out how to initialize the list with the array positions so I .added them
+            topLine.Add(slotMachine[0,0]);
+            topLine.Add(slotMachine[0,1]);
+            topLine.Add(slotMachine[0,2]);
+
             List<int> middleLine = new List<int>();
+            middleLine.Add(slotMachine[1,0]);
+            middleLine.Add(slotMachine[1,1]);
+            middleLine.Add(slotMachine[1,2]);
+
             List<int> bottomLine = new List<int>();
+            bottomLine.Add(slotMachine[2,0]);
+            bottomLine.Add(slotMachine[2,1]);
+            bottomLine.Add(slotMachine[2,2]);
 
-
             Console.Write("\t");
-            Console.Write(slotMachine[0, 0] + " ");
-            Console.Write(slotMachine[0, 1] + " ");
-            Console.WriteLine(slotMachine[0, 2]);
+            Console.Write(topLine[0]);
+            Console.Write(topLine[1]);
+            Console.WriteLine(topLine[2]);  
             Console.Write("\t");
-            Console.Write(slotMachine[1, 0] + " ");
-            Console.Write(slotMachine[1, 1] + " ");
-            Console.WriteLine(slotMachine[1, 2]);
+            Console.Write(middleLine[0]);
+            Console.Write(middleLine[1]);
+            Console.WriteLine(middleLine[2]);
             Console.Write("\t");
-            Console.Write(slotMachine[2, 0] + " ");
-            Console.Write(slotMachine[2, 1] + " ");
-            Console.WriteLine(slotMachine[2, 2]);
+            Console.Write(bottomLine[0]);
+            Console.Write(bottomLine[1]);
+            Console.WriteLine(bottomLine[2]);
         }
 
     }
