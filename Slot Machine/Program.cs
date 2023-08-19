ba50f5eeae02306e59c5c2add3a423ac89abdbc7
brandonshawfhl <142125786+brandonshawfhl@users.noreply.github.com>
2023-08-18 19:20:20 -0700

    added a do while loop to allow for the player to play as many times as they like

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -11,6 +11,7 @@ namespace Slot_Machine
         const char USER_YES_CHOICE = 'Y';
         static void Main(string[] args)
         {
+            Console.Clear();
             char playAgain = USER_YES_CHOICE;
             int moneyCount = 100;
             if (moneyCount <= 0)
@@ -24,69 +25,74 @@ namespace Slot_Machine
             Console.WriteLine("How much money would you like to bet?");
             int userBet = 0;
             int userWinnings = userBet * 3;
-            while (true)
+            do
             {
-                userBet = Convert.ToInt32(Console.ReadLine());
-                if (userBet <= 0)
+
+                while (true)
                 {
-                    Console.WriteLine("Please bet at least $1.");
-                }
-            }
-            moneyCount = moneyCount - userBet;
-
-            int[,] slotMachine = new int[COLUMN_SIZE, ROW_SIZE];
-
-            for (int verticalNumber = 0; verticalNumber < COLUMN_SIZE; verticalNumber++)
-            {
-                for (int horizontalNumber = 0; horizontalNumber < ROW_SIZE; horizontalNumber++)
-                {
-                    slotMachine[verticalNumber, horizontalNumber] = rng.Next(0, RANDOM_MAX);
-                }
-            }
-
-            Console.WriteLine("\t" + slotMachine[0, 0] + slotMachine[0, 1] + slotMachine[0, 2]);
-            Console.WriteLine("\t" + slotMachine[1, 0] + slotMachine[1, 1] + slotMachine[1, 2]);
-            Console.WriteLine("\t" + slotMachine[2, 0] + slotMachine[2, 1] + slotMachine[2, 2]);
-
-            for (int rowNumber = 0; rowNumber < ROW_SIZE; rowNumber++)
-            {
-                for (int columnNumber = rowNumber; columnNumber < COLUMN_SIZE; columnNumber++)
-                {
-                    if (slotMachine[columnNumber, rowNumber] != slotMachine[columnNumber + 1, rowNumber])
+                    userBet = Convert.ToInt32(Console.ReadLine());
+                    if (userBet <= 0)
                     {
-                        break;
+                        Console.WriteLine("Please bet at least $1.");
                     }
-                    Console.WriteLine($"Congratulations! Row {rowNumber} was a winner for you!");
-                    moneyCount = moneyCount + userWinnings;
                 }
-            }
+                moneyCount = moneyCount - userBet;
 
-            for (int columnNumber = 0; columnNumber < COLUMN_SIZE; columnNumber++)
-            {
-                for (int rowNumber = columnNumber; rowNumber < ROW_SIZE; rowNumber++)
+                int[,] slotMachine = new int[COLUMN_SIZE, ROW_SIZE];
+
+                for (int verticalNumber = 0; verticalNumber < COLUMN_SIZE; verticalNumber++)
                 {
-                    if (slotMachine[rowNumber, columnNumber] != slotMachine[rowNumber + 1, columnNumber])
+                    for (int horizontalNumber = 0; horizontalNumber < ROW_SIZE; horizontalNumber++)
                     {
-                        break;
+                        slotMachine[verticalNumber, horizontalNumber] = rng.Next(0, RANDOM_MAX);
                     }
-                    Console.WriteLine($"Congratulations! Column {columnNumber} was a winner for you!");
-                    moneyCount = moneyCount + userWinnings;
                 }
-            }
 
-            for (int diagonalNumber = 0; diagonalNumber < COLUMN_SIZE; diagonalNumber++)
-            {
-                if (slotMachine[diagonalNumber, diagonalNumber] != slotMachine[diagonalNumber + 1, diagonalNumber + 1])
-                    break;
-            }
-            Console.WriteLine($"Congratulations! A diagonal was a winner for you!");
-            moneyCount = moneyCount + userWinnings;
+                Console.WriteLine("\t" + slotMachine[0, 0] + slotMachine[0, 1] + slotMachine[0, 2]);
+                Console.WriteLine("\t" + slotMachine[1, 0] + slotMachine[1, 1] + slotMachine[1, 2]);
+                Console.WriteLine("\t" + slotMachine[2, 0] + slotMachine[2, 1] + slotMachine[2, 2]);
 
-            Console.WriteLine("Sorry. Not this time!");
+                for (int rowNumber = 0; rowNumber < ROW_SIZE; rowNumber++)
+                {
+                    for (int columnNumber = rowNumber; columnNumber < COLUMN_SIZE; columnNumber++)
+                    {
+                        if (slotMachine[columnNumber, rowNumber] != slotMachine[columnNumber + 1, rowNumber])
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"Congratulations! Row {rowNumber} was a winner for you!");
+                        moneyCount = moneyCount + userWinnings;
+                    }
+                }
 
-            Console.WriteLine("\n");
-            Console.WriteLine($"Would you like to play again?({USER_YES_CHOICE} or press any other key to exit the program)\n");
-            playAgain = Char.ToUpper(Console.ReadKey(true).KeyChar);
+                for (int columnNumber = 0; columnNumber < COLUMN_SIZE; columnNumber++)
+                {
+                    for (int rowNumber = columnNumber; rowNumber < ROW_SIZE; rowNumber++)
+                    {
+                        if (slotMachine[rowNumber, columnNumber] != slotMachine[rowNumber + 1, columnNumber])
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"Congratulations! Column {columnNumber} was a winner for you!");
+                        moneyCount = moneyCount + userWinnings;
+                    }
+                }
+
+                for (int diagonalNumber = 0; diagonalNumber < COLUMN_SIZE; diagonalNumber++)
+                {
+                    if (slotMachine[diagonalNumber, diagonalNumber] != slotMachine[diagonalNumber + 1, diagonalNumber + 1])
+                        break;
+                }
+                Console.WriteLine($"Congratulations! A diagonal was a winner for you!");
+                moneyCount = moneyCount + userWinnings;
+
+                Console.WriteLine("Sorry. Not this time!");
+
+                Console.WriteLine("\n");
+                Console.WriteLine($"Would you like to play again?({USER_YES_CHOICE} or press any other key to exit the program)\n");
+                playAgain = Char.ToUpper(Console.ReadKey(true).KeyChar);
+
+            } while (playAgain == USER_YES_CHOICE);
         }
     }
 }
